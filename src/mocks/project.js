04d383f43fd4343fc9ea02c932cc0635ae789eac
Chinh Le <chinh.dev11@gmail.com>
2020-05-17 05:03:15 -0400

    Add mocks

---
+++ src/mocks/project.js
@@ -0,0 +1,64 @@
+/* eslint quotes: ["error", "double"] */
+
+export const deleteInput = {
+  input: {
+    id: "0c6855ef-cc33-4790-af57-b5462e461835",
+  },
+};
+
+export const createInput = {
+  /* input: {
+    order: 6,
+    name: "Stock Trader",
+    type: "lab",
+    languages: ["Vue 2", "Google Web Services", "Bootstrap 4"],
+    image: "stock-trader-vue-640.png",
+    alt: "Stock Trader",
+    url: "https://stock-trader-vue-6bd8c.web.app/",
+  },
+  input: {
+    order: 5,
+    name: "Burger Builder",
+    type: "lab",
+    languages: ["React 16", "Google Web Services", "Bootstrap 4"],
+    image: "burger-builder-react-640.png",
+    alt: "Burger Builder",
+    url: "https://burger-builder-react-8801d.web.app/",
+  },
+  input: {
+    order: 4,
+    name: "Recipe Book",
+    type: "lab",
+    languages: ["Angular 7", "Google Web Services", "Bootstrap 4"],
+    image: "recipe-book-ng-640.png",
+    alt: "Recipe Book",
+    url: "https://recipe-book-ng-183d5.firebaseapp.com/",
+  },
+  input: {
+    order: 3,
+    name: "Videotron",
+    type: "work",
+    languages: ["AngularJS (v1.5)", "jQuery", "Java", "Oracle Commerce"],
+    image: "videotron-mobility-640-en.jpg",
+    alt: "Videotron",
+    url: "https://www.videotron.com/",
+  },
+  input: {
+    order: 2,
+    name: "clubillico",
+    type: "work",
+    languages: ["AngularJS (v1.6)", "Java", "Oracle"],
+    image: "clubillico-640-en.jpg",
+    alt: "clubillico",
+    url: "https://clubillico.videotron.com/",
+  }, */
+  input: {
+    order: 1,
+    name: "illicoweb",
+    type: "work",
+    languages: ["AngularJS (v1.6)", "jQuery", "Java"],
+    image: "illicoweb-640-en.jpg",
+    alt: "illicoweb",
+    url: "https://illicoweb.videotron.com/",
+  },
+};
