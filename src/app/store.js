0724ec2035342c87049dd3f1e938afa501e3e38b
Chinh Le <chinh.dev11@gmail.com>
2020-06-05 14:09:23 -0400

    Add nav open/close handler with store manangement

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
+import navReducer from './navSlice';
 
 export default configureStore({
   reducer: {
     auth: authReducer,
+    nav: navReducer,
   },
 });
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// import Button from 'react-bootstrap/Button';
+import { useSelector, useDispatch } from 'react-redux';
+import { navOpen, navClose, navIsOpen } from '../app/navSlice';
 import './Nav.scss';
 
-const Nav = () => (
-  <div className="Nav d-flex align-items-center">
-    <div className="navIcon navIcon--open">
-      <span />
-      <span />
-      <span />
+const Nav = () => {
+  const isOpen = useSelector(navIsOpen);
+  // console.log('isOpen: ', isOpen);
+  const dispatch = useDispatch();
+  // const [iconWidth, setIconWidth] = useState('');
+
+  /* useEffect(() => {
+    console.log('useEffect');
+    setIconWidth('w-100');
+  }, []); */
+
+  const navHandler = () => {
+    dispatch(isOpen ? navClose() : navOpen());
+  };
+
+  return (
+    <div className="Nav d-flex align-items-center">
+      {isOpen
+        ? (
+          <button onClick={navHandler} type="button" className="navIcon navIcon--close">
+            {/* <span className={`one ${iconWidth}`} /> */}
+            <span className="one" />
+            <span className="two" />
+          </button>
+        )
+        : (
+          <button type="button" onClick={navHandler} className="navIcon navIcon--open">
+            <span />
+            <span />
+            <span />
+          </button>
+        )}
     </div>
-    <div className="navIcon navIcon--close">
-      <span className="one" />
-      <span className="two" />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Nav;
