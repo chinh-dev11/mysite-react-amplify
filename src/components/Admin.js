43cb1c7c6fea7a2de7abad3c4b6488054a2db35a
Chinh Le <chinh.dev11@gmail.com>
2020-06-22 17:47:00 -0400

    Add admin create project item mutation

---
+++ src/components/Admin.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { createProject } from '../graphql/mutations';
+import projects from '../data/projects.json';
+
+const Admin = () => {
+  console.log('Admin');
+  const createItem = async (element) => {
+    const createNewItem = () => API.graphql(graphqlOperation(createProject, element));
+    const result = await createNewItem();
+    console.log(result);
+  };
+
+  const createHandler = () => {
+    projects.forEach((element) => {
+      console.log(element);
+      createItem(element);
+    });
+  };
+
+  useEffect(() => {
+    console.log('useEffect');
+  }, []);
+
+  return (
+    <div className="Admin">
+      <button type="button" onClick={createHandler}>Put Projects</button>
+    </div>
+  );
+};
+
+export default Admin;
